4031dd516d173bbe00cc9f682d816eff3118e24d
Forest Park <fpark@vizzn.ca>
2023-06-27 09:17:13 -0600

    Add new dependencies and update styles in App component and configure Vite to use Million compiler plugin

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,86 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import { block, For } from "million/react";
+import rainbowGradient from "rainbow-gradient";
 
-function App() {
-  const [count, setCount] = useState(0)
+type ColorProps = { colors: string[] };
 
+function ReactColors({ colors }: ColorProps) {
+  const minWidth = `${100.0 / colors.length}vw`;
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+      }}
+    >
+      {colors.map((color, index) => (
+        <div
+          key={[color, index].join("-")}
+          style={{
+            minHeight: "100vh",
+            minWidth,
+            backgroundColor: color,
+          }}
+        ></div>
+      ))}
+    </div>
+  );
 }
 
-export default App
+const MillionColors = block(({ colors }: ColorProps) => {
+  const minWidth = `${100.0 / colors.length}vw`;
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+      }}
+    >
+      <For each={colors}>
+        {(color) => (
+          <div
+            style={{
+              minHeight: "100vh",
+              minWidth,
+              backgroundColor: color,
+            }}
+          ></div>
+        )}
+      </For>
+    </div>
+  );
+});
+
+const rainbowColors = rainbowGradient(360).map(
+  ([r, g, b]) => `rgb(${r},${g},${b})`
+);
+
+function App() {
+  const [colors, setColors] = useState(
+    new Array(360 * 4)
+      .fill(0)
+      .map((_, i) => rainbowColors[i % rainbowColors.length])
+  );
+
+  useEffect(() => {
+    let mounted = true;
+    const update = () => {
+      if (mounted) {
+        setColors((colors) => {
+          const newColors = [...colors];
+          newColors.push(newColors.shift() as string);
+          return newColors;
+        });
+        requestAnimationFrame(update);
+      }
+    };
+    requestAnimationFrame(update);
+    return () => {
+      mounted = false;
+    };
+  }, []);
+
+  return <ReactColors colors={colors} />;
+}
+
+export default App;
